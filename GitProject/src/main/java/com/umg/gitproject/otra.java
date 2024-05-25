95a9eda3cb8ed1458622469f5d436288264b6e51
mperaltah1 <mperaltah1@miumg.edu.gt>
2024-05-25 15:00:56 -0600

    agregando clase

---
+++ GitProject/src/main/java/com/umg/gitproject/otra.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.umg.gitproject;
+
+/**
+ *
+ * @author mrph0
+ */
+public class otra {
+    public static void main(String[] args) {
+        System.out.println("Otra clase");
+    }
+}
